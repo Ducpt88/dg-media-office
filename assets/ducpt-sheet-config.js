0b1c2b21dddaecf34db7d0bfa49fc2c8b17d5b6e
DUCPT <admin@ducpt.com>
2026-07-22 23:54:37 +0700

    Configure signup sheet web app

--- assets/ducpt-sheet-config.js
+++ assets/ducpt-sheet-config.js
@@ -2,4 +2,4 @@
    Paste the Google Apps Script Web App /exec URL between the quotes.
    This file contains no secret. If you set an admin key in Apps Script,
    save that key inside Passport Settings on your own browser. */
-window.DUCPT_SIGNUP_SHEET_API = window.DUCPT_SIGNUP_SHEET_API || "";
+window.DUCPT_SIGNUP_SHEET_API = window.DUCPT_SIGNUP_SHEET_API || "https://script.google.com/macros/s/AKfycbwhlfuljWa6cnb5plFqbwzNwQu9LsbxVloXDcDLR7BROGFM6dHs5EfD_qp1c_1UDtZLmQ/exec";
